2a3895dc88e7a0258a62d97da4fdf8ae3064af78
Eduard K <kanashevsky1@ya.ru>
2022-06-17 03:52:02 +0400

    Урок: Макеты (Layout)

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,22 +1,16 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel | About</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <script src="{{ asset('js/app.js') }}"></script>
-</head>
-<body>
-<div class="container mt-4">
+@extends('layouts.app')
+
+<!-- Секция, содержимое которой обычный текст. -->
+@section('title', 'О блоге')
+
+<!-- Секция, содержащая HTML блок. Имеет открывающую и закрывающую часть. -->
+@section('content')
     <h1>О блоге</h1>
-    <p>Эксперименты с Ларавелем на Хекслете</p>
-    <!-- BEGIN (write your solution here) -->
-        @foreach ($team as $user)
+    <p>Эксперименты с Laravel на Хекслете</p>
+    @foreach ($team as $user)
+        <div>
             <p>Имя: {{ $user['name'] }}</p>
             <p>Позиция: {{ $user['position'] }}</p>
-        @endforeach
-    <!-- END -->
-</div>
-</body>
-</html>
+        </div>
+    @endforeach
+@endsection
